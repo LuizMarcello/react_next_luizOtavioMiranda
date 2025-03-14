d4eee57d91509fceac66855d1ebdbf38c1b05866
luizmarcello <luizmarcello.codes@gmail.com>
2025-03-14 11:17:18 -0300

    14.03.25_Testando

--- react_o_basico/src/templates/Home/index.jsx
+++ react_o_basico/src/templates/Home/index.jsx
@@ -6,7 +6,7 @@ import { loadPosts } from '../../utils/load.posts';
 import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
-/* Componente de classe */
+/* Este é um Componente de classe */
 class Home extends Component {
 
   state = {
